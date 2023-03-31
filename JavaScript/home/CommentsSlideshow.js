414383fb886cbb8dd363184ee27135d1285d8f36
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-04-01 01:40:41 +0530

    TaskTonic V1.9.6

--- JavaScript/home/CommentsSlideshow.js
+++ JavaScript/home/CommentsSlideshow.js
@@ -1,19 +1,62 @@
 const carousel = document.querySelector(".comment-carousel");
 const slides = document.querySelectorAll(".comment-slide");
 
+const up = document.querySelector(".comments-controller.up");
+const down = document.querySelector(".comments-controller.down");
+
 let currentSlide = 0;
 
 export default function CommentsSlideshow() {
-  ChangeSlides(5000);
+  ScrollUp();
+  ScrollDown();
 }
 
-function ChangeSlides(intervalDuration) {
-  setInterval(() => {
-    if (currentSlide === slides.length - 1) currentSlide = 0;
-    else ++currentSlide;
+function ScrollUp() {
+  up.addEventListener("click", () => {
+    if (currentSlide === 0) return;
+    --currentSlide;
 
+    alert(currentSlide);
+
+    ChangeSlides();
+  });
+}
+
+function ScrollDown() {
+  down.addEventListener("click", () => {
+    if (currentSlide === slides.length - 1) return;
+    ++currentSlide;
+
+    alert(currentSlide);
+
+    ChangeSlides();
+  });
+}
+
+function ChangeSlides() {
+  SlidesAnimation(0);
+
+  setTimeout(() => {
     slides.forEach((slide) => {
-      slide.style.translate = `${-currentSlide * carousel.clientWidth}px 0`;
+      slide.style.transform = `translateX(${
+        -currentSlide * carousel.clientWidth
+      }px)`;
     });
-  }, intervalDuration);
+  }, 400);
+
+  SlidesAnimation(800);
+}
+
+function SlidesAnimation(timeoutDuration) {
+  setTimeout(() => {
+    slides.forEach((slide) => {
+      if (slide.classList.contains("comment-hide")) {
+        slide.classList.remove("comment-hide");
+        slide.classList.add("comment-reveal");
+      } else {
+        slide.classList.remove("comment-reveal");
+        slide.classList.add("comment-hide");
+      }
+    });
+  }, timeoutDuration);
 }
